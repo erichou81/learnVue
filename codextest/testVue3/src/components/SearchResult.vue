73b7dbc2fb3b0aa8ebe5e3d3fa4cfc885f2b8a3a
erichou81 <houeric81@gmail.com>
2023-11-28 17:47:20 +0800

    解决script中没有ts 无法识别的问题 解决script中没有ts 无法识别的问题

--- codextest/testVue3/src/components/SearchResult.vue
+++ codextest/testVue3/src/components/SearchResult.vue
@@ -1,25 +1,23 @@
-<template>
-    <div class="search-result">
-      <!-- 在这里添加你的模板代码 -->
- 
-   
+<script setup lang="ts">
+// export default {
+//   name: "SearchResult",
+//   // 在这里添加你的组件选项，例如 data, methods 等
+// };
+</script>
 
-    </div> 
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-  </template>
-  
-  <script>
-  export default {
-    name: 'SearchResult',
-    // 在这里添加你的组件选项，例如 data, methods 等
-  }
-  </script>
-  
-  <style scoped>
-  /* 在这里添加你的样式 */
-  .search-result {
-    
-  }
-  </style>
+<template>
+  <div class="search-result">
+    <!-- 在这里添加你的模板代码 -->
+  </div>
+  <a href="https://vitejs.dev" target="_blank">
+    <img src="/vite.svg" class="logo" alt="Vite logo" />
+  </a>
+</template>
+
+
+
+<style scoped>
+/* 在这里添加你的样式 */
+.search-result {
+}
+</style>
