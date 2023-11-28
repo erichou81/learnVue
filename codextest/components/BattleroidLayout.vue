20ad4f1c39187614fe938f9f8964b7ea2d9312f4
erichou81 <houeric81@gmail.com>
2023-11-28 15:29:13 +0800

    使用poliot

---
+++ codextest/components/BattleroidLayout.vue
@@ -0,0 +1,59 @@
+<template id="aid">
+  <div class="d2" v-on:click="s">
+    <img v-bind:src="im" style="height: 40px; width: 60px" v-if="show" />
+  </div>
+</template>
+<style>
+.d2 {
+  width: 60px;
+  height: 40px;
+  background-color: pink;
+  display: inline-block;
+}
+</style>
+<script type="text/javascript">
+
+    // import MyComponentTemplate from './BattleroidLayout.vue';
+    //vue自定义构建组件
+    Vue.component("a-div", {
+      //模板位置
+      template: "#a-id",
+      //父组件向子组件传递内容
+      props: ["im"],
+
+      data: function () {
+        return {
+          show: true,
+        };
+      },
+      //根据调用的方法v-on:click="s"和属性v-if="show"显示隐藏
+      methods: {
+        s: function () {
+          this.show = !this.show;
+        },
+      },
+    });
+    var app = new Vue({
+      el: "#app",
+      data: {
+        imgg: [
+          "images-BMW/宝马_01.gif",
+          "images-BMW/宝马_02.gif",
+          "images-BMW/宝马_03.gif",
+          "images-BMW/宝马_04.gif",
+          "images-BMW/宝马_05.jpg",
+          "images-BMW/宝马_06.gif",
+          "images-BMW/宝马_07.gif",
+          "images-BMW/宝马_08.gif",
+          "images-BMW/宝马_09.gif",
+          "images-BMW/宝马_10.gif",
+          "images-BMW/宝马_11.gif",
+          "images-BMW/宝马_12.gif",
+          "images-BMW/宝马_13.gif",
+          "images-BMW/宝马_14.gif",
+          "images-BMW/宝马_15.gif",
+          "images-BMW/宝马_16.gif",
+        ],
+      },
+    });
+  </script>
---
+++ codextest/testVue3/src/components/SearchResult.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="search-result">
+      <!-- 在这里添加你的模板代码 -->
+ 
+   
+
+    </div> 
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SearchResult',
+    // 在这里添加你的组件选项，例如 data, methods 等
+  }
+  </script>
+  
+  <style scoped>
+  /* 在这里添加你的样式 */
+  .search-result {
+    
+  }
+  </style>
